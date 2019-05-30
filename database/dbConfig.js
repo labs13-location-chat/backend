7d2514938b89ab0988ed5a0dc38754caade41aa6
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-30 18:17:41 -0400

    Added env file to fix deployment erros

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile');
 
-const dbENV = process.env.db_env || 'development';
+const dbENV = process.env.DB_ENV || 'development';
 
 module.exports = knex(knexConfig[dbENV]);
