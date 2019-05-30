31b8d8df67f6cbb83ddf64bcff0d2c57b710261e
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-30 15:13:28 -0700

    init

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send('server working')
+});
+
+module.exports= server;
