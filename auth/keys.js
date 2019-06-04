de4912c482bd1829f0cb63c304f2330db184ee97
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-06-04 14:07:45 -0400

    Fixed the client secret

--- auth/keys.js
+++ auth/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
-    google: {
-        clientID: '474752355928-ksr86oi0a7uhbu8u9s37o2iqrct0rsj9.apps.googleusercontent.com',
-        clientSecret: 'AIzaSyBRsE9M2gl8DhiqmaFu4UxsD445fC1FSHs',
-    }
-}
+	google: {
+		clientID:
+			'474752355928-ksr86oi0a7uhbu8u9s37o2iqrct0rsj9.apps.googleusercontent.com',
+		clientSecret: 'lK4KpGDCpDw0s71b1oYPiNE0'
+	}
+};
