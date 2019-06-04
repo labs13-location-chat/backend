497cd808be828574b9b3bc4b193f73d43ac8f1b2
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-06-04 19:38:24 -0400

    setting up for heroku

--- knexfile.js
+++ knexfile.js
@@ -23,6 +23,13 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: ""
+    connection: "process.env.DATABASE_URL",
+
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
   }
 };
