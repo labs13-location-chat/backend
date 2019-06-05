79e5ef5bdd65fc37af6dc4121cc72ebc19e47341
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-06-05 17:49:36 -0400

    Added cookies session when logging in with oauth. Adds cookies to browser/device

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const passport = require('passport');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const cors = require('cors');
 // const connect = require('connect');
-// const cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session');
+const keys = require('../auth/keys');
 const userRouter = require('../routes/users/userRouter');
 const authRouter = require('../auth/authRouter');
 const chatRouter = require('../routes/chatrooms/chatRouter');
@@ -16,28 +19,37 @@ const server = express();
 // set view engine
 server.set('view engine', 'ejs');
 
+// cookie session -stores cookie in browser
+server.use(
+	cookieSession({
+		maxAge: 24 * 60 * 60 * 1000,
+		keys: [ keys.session.cookieKey ]
+	})
+);
+
 // initialize passport
 server.use(passport.initialize());
-// app.use(
-// 	cookieSession({
-// 		keys: [ 'secret1', 'secret2' ]
-// 	})
-// );
 server.use(passport.session());
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
 server.use(cors());
 
 // set up routes for google auth
 server.use('/auth', authRouter);
 server.use('/profile', profileRoutes);
 
-server.use(express.json());
 server.use('/api/users', userRouter);
 server.use('/api', authRouter);
 server.use('/api/chatrooms', chatRouter);
 server.use('/api/messages', messageRouter);
 
 server.get('/', (req, res) => {
-	res.send('Server Running...');
+	// res.send('Server is running...');
+
+	// renders ejs template
+	res.render('home', { user: req.user });
 });
 
 module.exports = server;
