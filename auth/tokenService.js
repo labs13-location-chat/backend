9fa92bdf3b361ad4ae27f198f3e934b9bc773038
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-06-10 17:43:21 -0400

    Token generation function

---
+++ auth/tokenService.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../auth/secrets');
+
+module.exports = {
+	generateToken
+};
+
+function generateToken(user) {
+	console.log('Needs a token:', user);
+
+	const payload = {
+		user: user.id,
+		email: user.email
+	};
+	const options = {
+		expiresIn: '30min'
+	};
+
+	return jwt.sign(payload, secrets.jwtSecret, options);
+}
