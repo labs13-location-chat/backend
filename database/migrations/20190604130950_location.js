2e599185a4c5ad9332b02b1a47fc1036babbae35
jvgiv <jvgiv12@gmail.com>
2019-06-28 00:17:22 -0400

    Have to do this to revert back postgres

---
+++ database/migrations/20190604130950_location.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("location", tbl => {
+    tbl.float("longitude").notNullable();
+    tbl.float("latitude").notNullable();
+    tbl.string("name").notNullable()
+    tbl
+      .integer("chatroom_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("chatrooms")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("location");
+};
