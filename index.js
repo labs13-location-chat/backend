979ca403d60aa2c47698937bd6d6892877fca9b4
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-07-29 17:11:13 -0400

    Switched port back to original after testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const server = require('./api/server');
 
-const port = process.env.PORT || (19002 && 5000);
+const port = process.env.PORT || 19002;
 server.listen(port, function() {
 	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
