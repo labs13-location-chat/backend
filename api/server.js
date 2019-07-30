38db268720ff9563df20534d91228a2d3c67dc18
Yeshi <tyeshi777@yahoo.com>
2019-07-30 15:36:05 -0700

    removed the view.ejs, twilio and commented out the session

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-const knexSessionStore = require("connect-session-knex")(session);
+// const knexSessionStore = require("connect-session-knex")(session);
 const passport = require("passport");
 const helmet = require("helmet");
 const morgan = require("morgan");
@@ -13,32 +13,28 @@ const chatRouter = require("../routes/chatrooms/chatRouter");
 const messageRouter = require("../routes/message/messageRouter");
 const profileRoutes = require("../auth/profileRouter");
 const googlePassport = require("../config/googlePassport");
-var twilioToken = require("../routes/twilio/twilioRoute");
 
 const server = express();
 
-// set view engine
-server.set("view engine", "ejs");
-
 // cookie session -stores cookie in browser
-server.use(
-  session({
-    secret: [keys.session.secret],
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      path: "/",
-      httpOnly: true,
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000
-    },
-    store: new knexSessionStore({
-      knex: require("../database/dbConfig"),
-      createTable: true,
-      clearInterval: 1000 * 60 * 15
-    })
-  })
-);
+// server.use(
+//   session({
+//     secret: [keys.session.secret],
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       path: "/",
+//       httpOnly: true,
+//       secure: false,
+//       maxAge: 24 * 60 * 60 * 1000
+//     },
+//     store: new knexSessionStore({
+//       knex: require("../database/dbConfig"),
+//       createTable: true,
+//       clearInterval: 1000 * 60 * 15
+//     })
+//   })
+// );
 
 // initialize passport
 server.use(passport.initialize());
@@ -63,7 +59,6 @@ server.use("/api/users", userRouter);
 server.use("/api", authRouter);
 server.use("/api/chatrooms", chatRouter);
 server.use("/api/messages", messageRouter);
-server.use("/api", twilioToken);
 
 server.get("/", (req, res) => {
   // res.send('Server is running...');
