abdfe3aa70c1d88b6083d77d5e291c0b440bf556
tyeshi181 <37460471+tyeshi181@users.noreply.github.com>
2019-08-01 11:00:22 -0700

    Update authRouter.js

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,77 +1,67 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 const router = express();
 
-router.get('/login', (req, res) => {
-	// console.log("REQUEST from /login route!!", req)
-	res.render('login', { users: req.user });
+router.get("/login", (req, res) => {
+  // console.log("REQUEST from /login route!!", req)
+  res.render("login", { users: req.user });
 });
 
 // auth with google
 router.get(
-	'/google',
-	passport.authenticate('google', {
-		scope: [ 'profile', 'email' ]
-	})
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
 );
 
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-	// console.log('USER', req.user,'req', res);
-	res.redirect('labs13localchat://login?user=' + JSON.stringify(req.user));
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  // console.log('USER', req.user,'req', res);
+  res.redirect("labs13localchat://login?user=" + JSON.stringify(req.user));
 });
 
 router.get(
-	'/google/callback',
-	passport.authenticate('google',	{
-		failureRedirect: '/login'
-	}),
-	function(req, res) {
-		res.redirect(
-			'labs13localchat://login?user=' + JSON.stringify(req.user)
-		);
-	}
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login"
+  }),
+  function(req, res) {
+    res.redirect("labs13localchat://login?user=" + JSON.stringify(req.user));
+  }
 );
 
-
-router.get('/logout', (req, res) => {
-  req.logout();
-  req.session.destroy((err) => {
-    res.clearCookie('connect.sid')
-    res.redirect("/auth/login')
-    
-  });
+router.get("/logout", async (req, res) => {
+  await req.logout();
+  await req.session.destroy();
+  await res.redirect("/auth/login");
 });
 
 router.get(
-	'/facebook',
-	passport.authenticate('facebook', {
-		scope: [ 'email' ]
-	})
+  "/facebook",
+  passport.authenticate("facebook", {
+    scope: ["email"]
+  })
 );
 
 router.get(
-	'/facebook/redirect',
-	passport.authenticate('facebook'),
-	(req, res) => {
-		console.log('USER', req.user);
-		res.redirect(
-			'labs13localchat://login?user=' + JSON.stringify(req.user)
-		);
-	}
+  "/facebook/redirect",
+  passport.authenticate("facebook"),
+  (req, res) => {
+    console.log("USER", req.user);
+    res.redirect("labs13localchat://login?user=" + JSON.stringify(req.user));
+  }
 );
 
 router.get(
-	'/facebook/callback',
-	passport.authenticate('facebook', {
-		failureRedirect: '/login'
-	}),
-	function(req, res) {
-		res.redirect(
-			'labs13localchat://login?user=' + JSON.stringify(req.user)
-		);
-	}
+  "/facebook/callback",
+  passport.authenticate("facebook", {
+    failureRedirect: "/login"
+  }),
+  function(req, res) {
+    res.redirect("labs13localchat://login?user=" + JSON.stringify(req.user));
+  }
 );
 
 module.exports = router;
